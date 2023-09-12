bb1bdcafdb4be9461ce98a3a10c2e51b3de1fbec
이신우 <128461193+Satellite1218@users.noreply.github.com>
2023-09-13 00:05:51 +0900

    ReadLine

---
+++ 입력받기.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            string b, n;
+
+            //문자열
+            b = Console.ReadLine(); // 입력
+            Console.WriteLine(b);   // 출력
+
+            //정수
+            n = Console.ReadLine(); // 입력
+            a = Convert.ToInt32(n); //정수변환
+            Console.WriteLine(a);    // 출력
+        }
+    }
+}
