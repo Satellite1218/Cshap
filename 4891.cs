b79b5eb3a764179e1c2a68a0b92ac0f7495e9a81
이신우 <128461193+Satellite1218@users.noreply.github.com>
2024-01-30 20:46:31 +0900

    codeup 4891 c#
    codeup에는 c# 제출기능이 없어 c++ 변환후 제출하였습니다

---
+++ 4891.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, max = 0, min = 1000;
+            string n = Console.ReadLine();
+            a = int.Parse(n);
+            int[] arr = new int[a];
+
+            /*중요 포인트*/
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+
+            for (int i = 0; i < a; i++)
+            {
+                arr[i] = int.Parse(numbers[i]);
+            }
+            /*-----------*/
+
+            for (int i = 0; i < a; i++)
+            {
+                if (arr[i] >= max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] <= min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine("{0}", max - min);
+        }
+    }
+}
