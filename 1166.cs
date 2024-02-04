f6bd57012fba96b2d7e5e312b26e3676a34ff089
이신우 <128461193+Satellite1218@users.noreply.github.com>
2024-02-04 22:03:49 +0900

    codeup 1166 c#

---
+++ 1166.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, max = 0, min = 1000;
+            string n = Console.ReadLine();
+            a = int.Parse(n);
+            if (a % 4 == 0 && a % 100 != 0)
+            {
+                Console.WriteLine("Leap");
+            }
+            else if (a % 400 == 0)
+            {
+                Console.WriteLine("Leap");
+            }
+            else
+            {
+                Console.WriteLine("Normal");
+            }
+        }
+    }
+}
